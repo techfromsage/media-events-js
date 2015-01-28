d96cfa6b87469f4d279e6b6f10f570bcc14f339f
Mark Wallsgrove <mw@talis.com>
2015-01-28 21:05:40 +0000

    Removed magic numbers
    Removed magic numbers and replaced them with the comparison
    between time past & time past within the playback.

--- videoAnalytics.js
+++ videoAnalytics.js
@@ -2,14 +2,24 @@ function VideoAnalytics(videoElement, flushEvent) {
     videoAnalytics = this;
 
     this.intervalLength = 10000;
+    this.lastKnownPlaybackTime = 0;
     this.lastKnownTime = 0;
     this.playedFrom = 0;
     this.expectedInterval = this.intervalLength;
-    this.forwardSeekTolerence = 300;
-    this.backwardsSeekTolerence = -300;
-    this.restartingTolerence = 300;
     this.endTime = -1;
-    
+    this.hasEnded = true;
+    this.hasStarted =false;
+ 
+    videoElement.addEventListener("play", function() {
+       if(videoAnalytics.hasEnded === true) {
+           videoAnalytics.hasEnded = false;
+           videoAnalytics.hasStarted = true;
+           if(videoElement.currentTime * 1000 < videoAnalytics.intervalLength) {
+               flushEvent(videoAnalytics.play());
+           }
+       }
+    });    
+
     videoElement.addEventListener("timeupdate", function() {
         if(!videoElement.paused && !videoElement.seeking) {
             flushEvent(videoAnalytics.tick(videoElement.currentTime * 1000));
@@ -28,70 +38,90 @@ function VideoAnalytics(videoElement, flushEvent) {
 }
 
 VideoAnalytics.prototype.setStartTime = function(startTime) {
-    if(this.lastKnownTime !== 0 || this.playedFrom !== 0) {
+    if(this.lastKnownPlaybackTime !== 0 || this.playedFrom !== 0) {
         throw 'Cannot set start time after starting';
     }
 
-    this.lastKnownTime = startTime;
+    this.lastKnownPlaybackTime = startTime;
     this.playedFrom = startTime;
 }
 
-VideoAnalytics.prototype.tick = function(time) {
-    var timeDiff = time - this.lastKnownTime;
+VideoAnalytics.prototype.play = function() {
+    this.lastKnownTime = (new Date).getTime();
+    return new Array({type: 'view'});
+}
+
+VideoAnalytics.prototype.tick = function(playbackTime) {
+    var currentTime = (new Date).getTime();
+    var playbackDiff = playbackTime - this.lastKnownPlaybackTime;
+    var timeDiff = currentTime - this.lastKnownTime;
     var tickEvents = Array();
 
-    if(this.endTime === this.lastKnownTime && time <= this.restartingTolerence) {
+    if (playbackDiff > timeDiff) {
+        var difference = playbackDiff - timeDiff;
+    } else {
+        var difference = timeDiff - playbackDiff;
+    }
+
+    console.info({ difference : difference });
+
+    if (this.endTime === this.lastKnownPlaybackTime && playbackTime <= this.intervalLength) {
         // restarting the stream
         this.expectedInterval = this.intervalLength;
         this.playedFrom = 0;
-    } else if(timeDiff > this.forwardSeekTolerence) {
-        // seeked forward 
-        tickEvents.push({
-            start: this.lastKnownTime,
-            end: time,
-            name: 'forward',
-            type: 'seek'
-        });
-
-        if(this.playedFrom !== this.lastKnownTime) {
+    } else if (difference > 50) {
+        if (playbackDiff > 0) {
+            // seeked forward 
             tickEvents.push({
-                start: this.playedFrom, 
-                end: this.lastKnownTime,
+                start: this.lastKnownPlaybackTime,
+                end: playbackTime,
                 name: 'forward',
-                type: 'tick',
-                premature: true
+                type: 'seek'
             });
-        }
-
-        this.expectedInterval = (((time / this.intervalLength) | 0) + 1) * this.intervalLength;
-        this.playedFrom = time;
-    } else if(timeDiff < this.backwardsSeekTolerence) {
-        // seeked backwards
-        tickEvents.push({
-            start: this.lastKnownTime,
-            end: time,
-            name: 'backward',
-            type: 'seek'
-        });
-
-        if(this.playedFrom !== this.lastKnownTime) {
+    
+            if (this.playedFrom !== this.lastKnownPlaybackTime) {
+                tickEvents.push({
+                    start: this.playedFrom, 
+                    end: this.lastKnownPlaybackTime,
+                    index: this.expectedInterval,
+                    name: 'forward',
+                    type: 'tick',
+                    premature: true
+                });
+            }
+    
+            this.expectedInterval = (((playbackTime / this.intervalLength) | 0) + 1) * this.intervalLength;
+            this.playedFrom = playbackTime;
+        } else if (playbackDiff < 0) {
+            // seeked backwards
             tickEvents.push({
-                start: this.playedFrom, 
-                end: this.lastKnownTime,
+                start: this.lastKnownPlaybackTime,
+                end: playbackTime,
                 name: 'backward',
-                type: 'tick',
-                premature: true
+                type: 'seek'
             });
+    
+            if (this.playedFrom !== this.lastKnownPlaybackTime) {
+                tickEvents.push({
+                    start: this.playedFrom, 
+                    end: this.lastKnownPlaybackTime,
+                    index: this.expectedInterval,
+                    name: 'backward',
+                    type: 'tick',
+                    premature: true
+                });
+            }
+    
+            this.expectedInterval = (((playbackTime / this.intervalLength) | 0) + 1) * this.intervalLength;
+            this.playedFrom = playbackTime;
         }
-
-        this.expectedInterval = (((time / this.intervalLength) | 0) + 1) * this.intervalLength;
-        this.playedFrom = time;
-    } else if(time >= this.expectedInterval) {
+    } else if (playbackTime >= this.expectedInterval) {
         // general tick over the index bounderies
 
         tickEvents.push({
             start: this.playedFrom, 
             end: this.expectedInterval, 
+            index: this.expectedInterval,
             name: 'tick',
             type: 'tick',
             premature: false
@@ -100,9 +130,10 @@ VideoAnalytics.prototype.tick = function(time) {
         this.playedFrom = this.expectedInterval;
         this.expectedInterval += this.intervalLength;
     }
-
-    this.lastKnownTime = time;
-    if(tickEvents.length > 0) {
+ 
+    this.lastKnownTime = currentTime;
+    this.lastKnownPlaybackTime = playbackTime;
+    if (tickEvents.length > 0) {
         return tickEvents;
     }
 
@@ -112,27 +143,29 @@ VideoAnalytics.prototype.tick = function(time) {
 VideoAnalytics.prototype.pause = function() {
     var events = Array({
         start: this.playedFrom, 
-        end: this.lastKnownTime,
+        end: this.lastKnownPlaybackTime,
+        index: this.expectedInterval,
         name: 'pause',
         type: 'tick',
         premature: true
     }, {
-        time: this.lastKnownTime,
+        time: this.lastKnownPlaybackTime,
         type: 'pause',
         name: 'paused'
     });
 
-    this.playedFrom = this.lastKnownTime;
+    this.playedFrom = this.lastKnownPlaybackTime;
     return events;
 }
 
-VideoAnalytics.prototype.end = function(time) {
-    this.endTime = time;
-    this.lastKnownTime = time;
+VideoAnalytics.prototype.end = function(playbackTime) {
+    this.endTime = playbackTime;
+    this.lastKnownPlaybackTime = playbackTime;
 
     return Array({
         start: this.playedFrom, 
-        end: time,
+        end: playbackTime,
+        index: playbackTime,
         name: 'end',
         type: 'tick',
         premature: true
